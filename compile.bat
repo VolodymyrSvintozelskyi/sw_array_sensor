fe8c1a0a3351c2bac57283a86f6da6d2ec7cc467
VolodymyrSvintozelskyi <volodymyrsvintozelskyi@gmail.com>
2022-05-30 14:54:05 +0300

    bugfix

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 python -m eel .\main.py .\www\
-xcopy .\smu_drivers\ .\dist\main\ /E
-xcopy .\led_drivers\ .\dist\main\ /E
+xcopy .\smu_drivers\ .\dist\main\smu_drivers\ /E
+xcopy .\led_drivers\ .\dist\main\led_drivers\ /E
---
+++ dist/main/led_drivers/test_led_driver.py
@@ -0,0 +1,8 @@
+class LED:
+    def __init__(self, port) -> None:
+        print("LED open: ", port)
+    def setCurrent(self, i):
+        pass
+        # print("Apply LED I:",i)
+    def disconnect(self):
+        pass
