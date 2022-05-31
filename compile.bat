fa44e0592883f10804418404c2be9e7e8bf3b815
VolodymyrSvintozelskyi <volodymyrsvintozelskyi@gmail.com>
2022-05-31 18:27:42 +0300

    Recompile

--- compile.bat
+++ compile.bat
@@ -1,4 +1,5 @@
 @RD /S /Q "./dist"
+@RD /S /Q "./build"
 python -m eel .\main.py .\www\ --hiddenimport minimalmodbus --onefile --icon .\www\favicon.ico --name "Sensor array"
 xcopy .\smu_drivers\ .\dist\smu_drivers\ /E
 xcopy .\led_drivers\ .\dist\led_drivers\ /E
